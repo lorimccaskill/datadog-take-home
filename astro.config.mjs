65897e1d160fd28b963523f4a8a0752b0091a4ef
Lori McCaskill <lori.mccaskill@gmail.com>
2024-06-02 21:42:50 -0400

    attempt glidejs import fix (used in carousel section) , remove old console.log (customer section)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 import alpinejs from "@astrojs/alpinejs";
 
+import glide from '@glidejs/glide';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), glide]
 });
